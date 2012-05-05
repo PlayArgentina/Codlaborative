f5d767079de8283607af8eac9589ddb235d7aaeb
Jorge <jaliss@gmail.com>
2012-05-05 14:49:14 -0300

    added sign in and editor layout

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,13 +2,27 @@ package controllers
 
 import play.api._
 import libs.json.JsValue
+import play.api.data.Forms._
+import data.Form
 import play.api.mvc._
 import models.CodePad
 
 object Application extends Controller {
-  
+  val loginForm = Form( "username" -> nonEmptyText)
+
   def index = Action {
-    Ok("")
+    Ok(views.html.index())
+  }
+
+  def signIn = Action { implicit request =>
+    loginForm.bindFromRequest().fold (
+      formWithErrors => BadRequest(views.html.index()),
+      user => Redirect( routes.Application.editor(user))
+    )
+  }
+
+  def editor(username: String) = Action {
+    request => Ok(views.html.editor(username))
   }
 
   def join(coder: String) = WebSocket.async[JsValue] { request  =>
