d3548a97cc9e60be0c7bd68a49a51586c45d8600
Fernando Rodriguez Olivera <frodriguez@gmail.com>
2012-05-05 14:32:38 -0300

    Added actor for code pad (based on play samples)

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,18 @@
 package controllers
 
 import play.api._
+import libs.json.JsValue
 import play.api.mvc._
+import models.CodePad
 
 object Application extends Controller {
   
   def index = Action {
-    Ok(views.html.index())
+    Ok("")
+  }
+
+  def join(coder: String) = WebSocket.async[JsValue] { request  =>
+    CodePad.join(coder)
   }
   
 }
---
+++ app/models/Message.scala
@@ -0,0 +1,21 @@
+package models
+
+import play.api.libs.iteratee.Enumerator
+import play.api.libs.json.JsValue
+
+trait Message
+
+trait Command extends Message {
+  def username: String
+}
+
+case class Join(coder: String) extends Message
+
+case class Quit(coder: String) extends Message
+
+case class Connected(enumerator: Enumerator[JsValue])
+
+case class Append(username: String, code: String) extends Command
+
+case class Edit(username: String, code: String) extends Command
+
